0823d1e84593a8b181f38a82ffca4260ed398ef9
RohanVr04 <verma.rohan22@gmail.com>
2026-06-08 21:49:48 +0530

    Initially deploying the files

---
+++ Project/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import joblib
+
+model = joblib.load('best_model.pkl')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/events')
+def events():
+    return "You are on the events page."
+
+@app.route('/Stories')
+def stories():
+    return render_template('stories.html')
+
+@app.route('/Topics')
+def topics():
+    return render_template('topics.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Process the form data here
+    preg = eval(request.form.get('preg'))
+    plas = eval(request.form.get('plas'))
+    pres = eval(request.form.get('pres'))
+    skin = eval(request.form.get('skin'))
+    test = eval(request.form.get('test'))
+    mass = eval(request.form.get('mass'))
+    pedi = eval(request.form.get('pedi'))
+    age = eval(request.form.get('age'))
+
+    # Make a prediction using the loaded model
+    prediction = model.predict([[preg, plas, pres, skin, test, mass, pedi, age]])
+
+    if(prediction[0]==0):
+        return render_template('non_diabetic.html')
+    else:
+        return render_template('diabetic.html')
+
+app.run()
